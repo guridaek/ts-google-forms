e3ecdb503919fd2b641ce61a7b7d6f40ade3014b
guridaek <xylitol79@gmail.com>
2024-02-01 12:26:56 +0900

    feat: reset css

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router.tsx";
+import GlobalStyle from "./styles/GlobalStyles.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <GlobalStyle />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
